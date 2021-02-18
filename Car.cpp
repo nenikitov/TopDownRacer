ed9b66262eeb766e7602155a4c5f125de956d4a6
nenikitov <nenikitov@mail.ru>
2021-02-18 10:47:11 -0500

    Added comments and region

--- Car.cpp
+++ Car.cpp
@@ -1,6 +1,12 @@
 #include "Car.h"
 #include <iostream>
 
+/// <summary>
+/// Initialize the car object
+/// </summary>
+/// <param name="player">The player that will control the car</param>
+/// <param name="carShape">The rectangle that represents the car</param>
+/// <returns></returns>
 Car::Car(Player& player, sf::RectangleShape& carShape) : carShape(carShape), player(player)
 {
 	this->player = player;
@@ -9,40 +15,67 @@ Car::Car(Player& player, sf::RectangleShape& carShape) : carShape(carShape), pla
 	this->angle = carShape.getRotation() / -180 * 3.14159265358979323846;
 }
 
+/// <summary>
+/// Update car physics. This will update the car's location and rotation using the player's controls
+/// </summary>
+/// <param name="frameDelta">The time difference in between the frames. It is used to untie the physics from the framerate</param>
 void Car::physicsUpdate(double frameDelta)
 {
-	// Position and rotation sync
+	#pragma region Sync car angle and position
 	this->position = carShape.getPosition();
 	this->angle = carShape.getRotation() / -180 * 3.14159265358979323846;
+	#pragma endregion
 
-	// Current engine force is calculated from player controls and engine force
-	double currentEngineForce = player.forward * (player.forward > 0 ? engineForceFWD : engineForceBWD);
+	#pragma region Interpret player controls
+	double currentEngineForce;
+	if (player.forward == 1)
+		currentEngineForce = engineForceFWD;
+	else if (player.forward == -1)
+		currentEngineForce = -engineForceBWD;
+	else
+		currentEngineForce = 0;
+	#pragma endregion
 
+	#pragma region Forces that affect the car moving in a straight line
 	// Create useful variables for calculating forces
 	double speed = sqrt(this->velocity.x * this->velocity.x + this->velocity.y * this->velocity.y);
 	sf::Vector2f forwardVector(cos(this->angle), sin(this->angle));
 	sf::Vector2f speedForwardVector(speed == 0 ? 0 : velocity.x / speed, speed == 0 ? 0 : velocity.y / speed);
 
-	// Calculate all the forces that affect the car moving in a straight line
-	sf::Vector2f ftraction;
+	// Drag
 	sf::Vector2f fdrag(-this->cdrag * this->velocity.x * speed, -this->cdrag * this->velocity.y * speed);
+
+	// Roll
 	sf::Vector2f froll(this->croll * this->velocity.x, this->croll * this->velocity.y);
-	// Traction can be breaking force when the player is breaking or it can be traction force when the player is moving
+
+	// Traction
+	sf::Vector2f ftraction;
+	// Can be breaking force if the player is breaking
 	if (this->player.stop && speed > 0.25)
 		ftraction = sf::Vector2f(-speedForwardVector.x * this->cbreak, -speedForwardVector.y * this->cbreak);
 	else
 		ftraction = sf::Vector2f(currentEngineForce * forwardVector.x, currentEngineForce * forwardVector.y);
+
+	// The sum of these forces
 	sf::Vector2f flong = ftraction + fdrag + froll;
 
-	// Apply forces to position
+	// Acceleration from the sum
 	sf::Vector2f acceleration = sf::Vector2f(flong.x / this->mass, flong.y / this->mass);
 
+	// Velocity from acceleration
 	this->velocity.x = this->velocity.x + frameDelta * acceleration.x;
 	this->velocity.y = this->velocity.y + frameDelta * acceleration.y;
 
+	// Position from velocity
 	this->position.x = this->position.x + frameDelta * this->velocity.x;
 	this->position.y = this->position.y + frameDelta * this->velocity.y;
+	#pragma endregion
 
-	// Apply final position
+	#pragma region Apply position
 	carShape.setPosition(this->position);
+	#pragma endregion
+	
+	#pragma region Apply rotation
+	// TODO
+	#pragma endregion
 }
--- TopDownRacer.cpp
+++ TopDownRacer.cpp
@@ -5,41 +5,57 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "Top down racer", sf::Style::Close);
+    // Create a window that can't be resized
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Top down racer", sf::Style::Close);
+    // Create Delta clock for counting frame delta to untie physics from framerate
     sf::Clock deltaClock;
 
-    sf::RectangleShape carShape(sf::Vector2f(50,100));
+    #pragma region Initialize the car shape
+    sf::RectangleShape carShape(sf::Vector2f(25,50));
     carShape.setFillColor(sf::Color::Red);
-    carShape.setOrigin(25, 50);
+    carShape.setOrigin(carShape.getSize().x / 2, carShape.getSize().y / 2);
     carShape.setPosition(800, 450);
     carShape.setRotation(45);
+    #pragma endregion
 
+    // Create the player
     Player player;
+    // Initialize the car
     Car car(player, carShape);
 
+    #pragma region Each frame when the window is open
     while (window.isOpen())
     {
+        // Create an event
         sf::Event event;
+        // Get an event from the queue of current events
         while (window.pollEvent(event))
         {
-            // Key pressed
+            // If a key is pressed
             if (event.type == sf::Event::KeyPressed)
                 player.keyEvent(event.key.code, true);
-            // Key released
+            // If a key is released
             if (event.type == sf::Event::KeyReleased)
                 player.keyEvent(event.key.code, false);
-            // Close window
+            // If the window is closed
             if (event.type == sf::Event::Closed)
                 window.close();
         }
         
+        // Get the time between the frames
         sf::Time deltaTime = deltaClock.restart();
+
+        // Update the car physics
         car.physicsUpdate(deltaTime.asSeconds());
         
+        #pragma region Rendering
         window.clear();
         window.draw(carShape);
         window.display();
+        #pragma endregion
     }
+    #pragma endregion
 
+    // End of the program
     return 0;
 }
