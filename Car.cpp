8eadef61c1bc7781d19be9514e26620a87b24b12
nenikitov <nenikitov@mail.ru>
2021-02-19 19:17:40 -0500

    Changing car handling
    I completely deleted car handling I had before and started reworking it.

--- Car.cpp
+++ Car.cpp
@@ -9,10 +9,11 @@
 /// <returns></returns>
 Car::Car(Player& player, sf::RectangleShape& carShape) : carShape(carShape), player(player)
 {
-	this->player = player;
-	this->carShape = carShape;
+	//this->player = player;
+	//this->carShape = carShape;
 	this->position = carShape.getPosition();
 	this->angle = carShape.getRotation() / -180 * 3.14159265358979323846;
+	std::cout << this->HALF_WIDTH;
 }
 
 /// <summary>
@@ -27,6 +28,7 @@ void Car::physicsUpdate(double frameDelta)
 	#pragma endregion
 
 	#pragma region Interpret player controls
+	/*
 	double currentEngineForce;
 	if (player.forward == 1)
 		currentEngineForce = engineForceFWD;
@@ -34,42 +36,13 @@ void Car::physicsUpdate(double frameDelta)
 		currentEngineForce = -engineForceBWD;
 	else
 		currentEngineForce = 0;
+		*/
 	#pragma endregion
 
-	#pragma region Forces that affect the car moving in a straight line
-	// Create useful variables for calculating forces
-	double speed = sqrt(this->velocity.x * this->velocity.x + this->velocity.y * this->velocity.y);
-	sf::Vector2f forwardVector(cos(this->angle), sin(this->angle));
-	sf::Vector2f speedForwardVector(speed == 0 ? 0 : velocity.x / speed, speed == 0 ? 0 : velocity.y / speed);
+	#pragma region Moving in a straight line
 
-	// Drag
-	sf::Vector2f fdrag(-this->cdrag * this->velocity.x * speed, -this->cdrag * this->velocity.y * speed);
-
-	// Roll
-	sf::Vector2f froll(this->croll * this->velocity.x, this->croll * this->velocity.y);
-
-	// Traction
-	sf::Vector2f ftraction;
-	// Can be breaking force if the player is breaking
-	if (this->player.stop && speed > 0.25)
-		ftraction = sf::Vector2f(-speedForwardVector.x * this->cbreak, -speedForwardVector.y * this->cbreak);
-	else
-		ftraction = sf::Vector2f(currentEngineForce * forwardVector.x, currentEngineForce * forwardVector.y);
-
-	// The sum of these forces
-	sf::Vector2f flong = ftraction + fdrag + froll;
-
-	// Acceleration from the sum
-	sf::Vector2f acceleration = sf::Vector2f(flong.x / this->mass, flong.y / this->mass);
-
-	// Velocity from acceleration
-	this->velocity.x = this->velocity.x + frameDelta * acceleration.x;
-	this->velocity.y = this->velocity.y + frameDelta * acceleration.y;
-
-	// Position from velocity
-	this->position.x = this->position.x + frameDelta * this->velocity.x;
-	this->position.y = this->position.y + frameDelta * this->velocity.y;
 	#pragma endregion
+
 
 	#pragma region Apply position
 	carShape.setPosition(this->position);
--- Car.h
+++ Car.h
@@ -5,20 +5,87 @@
 class Car
 {
 	public:
+		/// <summary>
+		/// Initialize new car object
+		/// </summary>
+		/// <param name="player">The reference to the player that will control the car</param>
+		/// <param name="carShape">The reference to the rectangle shape that represents the car</param>
+		/// <returns>Initialized car</returns>
 		Car(Player& player, sf::RectangleShape& carShape);
+		/// <summary>
+		/// Take player controls, update the car physics and move the shape
+		/// </summary>
+		/// <param name="frameDelta">The difference between frames (in seconds) so the physics are untied from the framerate</param>
 		void physicsUpdate(double frameDelta);
 	private:
+		// References
+		/// <summary>
+		/// The reference to the player
+		/// </summary>
 		Player& player;
+		/// <summary>
+		/// The reference to the rendered object
+		/// </summary>
 		sf::RectangleShape& carShape;
 
-		sf::Vector2f position = sf::Vector2f(0, 0);
-		sf::Vector2f velocity = sf::Vector2f(0, 0);
+		// Variables that will be updated each tick during phisics simulation
+		/// <summary>
+		/// The angle of the car in radians
+		/// </summary>
 		double angle = 0;
+		/// <summary>
+		/// The position of the car in world space
+		/// </summary>
+		sf::Vector2f position = sf::Vector2f(0, 0);
+		/// <summary>
+		/// The velocity of the car in the world space
+		/// </summary>
+		sf::Vector2f velocity = sf::Vector2f(0, 0);
+		/// <summary>
+		/// Velocity of the car in local space
+		/// </summary>
+		sf::Vector2f velocityLocal = sf::Vector2f(0, 0);
+		/// <summary>
+		/// Acceleration of the car in the world space
+		/// </summary>
+		sf::Vector2f acceleration = sf::Vector2f(0, 0);
+		/// <summary>
+		/// Acceleration of the car in the local space
+		/// </summary>
+		sf::Vector2f accelerationLocal = sf::Vector2f(0, 0);
+		/// <summary>
+		/// The scalar that represents the velocity
+		/// </summary>
+		double velocityScalar = 0;
+		/// <summary>
+		/// The velocity of the rotation of the car in radians
+		/// </summary>
+		double angularVelocity = 0;
+		/// <summary>
+		/// The steer of the wheels in radians
+		/// </summary>
+		double steer = 0;
 
-		double engineForceFWD = 500000;
-		double engineForceBWD = 200000;
-		double mass = 1439;
-		double cdrag = 0.29;
-		double croll = 12.8;
-		double cbreak = 400000;
+		// Car parameters
+		const double GRAVITY = 9.81;
+		const double MASS = 1400;
+		const double INERTIA = 1;
+		const double HALF_WIDTH = carShape.getSize().x / 2;
+		const double CG_TO_FRONT = carShape.getSize().y / 1.8;
+		const double CG_TO_BACK = CG_TO_FRONT;
+		const double CG_TO_FRONT_AXLE = carShape.getSize().y / 1.5;
+		const double CG_TO_BACK_AXLE = carShape.getSize().y / 1.5;
+		const double CG_HEIGHT = 10;
+		const double WHEEL_RADIUS = 5;
+		const double TYPE_GRIP = 1.5;
+		const double LOCK_GRIP = 0.7;
+		const double ENGINE_FORCE = 8000;
+		const double BRAKE_FORCE = 12000;
+		const double E_BRAKE_FORCE = BRAKE_FORCE / 2.5;
+		const double WEIGHT_TRANSFER_VALUE = 0.3;
+		const double MAX_STEER = 0.5;
+		const double CORNER_STIFFNESS_FRONT = 5;
+		const double CORNER_STIFFNESS_BACK = 5;
+		const double AIR_RESISTANCE = 2.5;
+		const double ROLL_RESISTANCE = 8;
 };
